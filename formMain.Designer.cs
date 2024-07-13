c85862a30d3e7917e2c3fc6d38616dfe557484b0
Ufuk Mert Arslan <ufukmertarslan@outlook.com>
2024-07-13 16:48:49 +0300

    Proje dosyası ekle.

---
+++ formMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FileFolderList
+{
+    partial class formMain
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn_Cancel.Location = new System.Drawing.Point(12, 12);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(240, 47);
+            this.btn_Cancel.TabIndex = 0;
+            this.btn_Cancel.Text = "Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(264, 71);
+            this.Controls.Add(this.btn_Cancel);
+            this.Name = "formMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Folder List";
+            this.Shown += new System.EventHandler(this.formMain_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Cancel;
+    }
+}
+
---
+++ formMain.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileFolderList
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+        string newPath(string fPath)
+        {
+            return Path.Combine(currentDirectory,
+                Path.GetFileNameWithoutExtension(fPath) + String.Format("{0:_MM_dd_yyyy__h_mm_ss}",
+                File.GetLastWriteTime(fPath)) + ".txt");
+        }
+        string currentDirectory = "";
+        Thread th;
+
+        void list()
+        {
+            try
+            {
+                currentDirectory = Directory.GetCurrentDirectory();
+                string[] files = Directory.GetFiles(currentDirectory, ".", SearchOption.AllDirectories);
+                string[] directories = Directory.GetDirectories(currentDirectory,"*", SearchOption.AllDirectories);
+                string fPath = Path.Combine(currentDirectory, "filepaths.txt");
+                if (File.Exists(fPath))
+                {
+                    File.Move(fPath, newPath(fPath));
+                }
+                fPath = Path.Combine(currentDirectory, "documentsname.txt");
+                if (File.Exists(fPath))
+                {
+                    File.Move(fPath, newPath(fPath));
+                }
+                using (StreamWriter sw = new StreamWriter(new FileStream(Path.Combine(currentDirectory, "filepaths.txt"), FileMode.CreateNew), Encoding.UTF8))
+                {
+                    foreach (string value in files)
+                    {
+                        sw.WriteLine(value);
+                    }
+                }
+                using (StreamWriter sw = new StreamWriter(new FileStream(Path.Combine(currentDirectory, "documentsname.txt"), FileMode.CreateNew), Encoding.UTF8))
+                {
+                    sw.WriteLine("ROOT:" + currentDirectory + "\nSubfolders:");
+                    foreach (string value in directories)
+                    {
+                        sw.WriteLine(value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured: " + ex.Message);
+            }
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+            th.Abort();
+        }
+
+        private void formMain_Shown(object sender, EventArgs e)
+        {
+            Application.DoEvents();
+            th = new Thread(list);
+            th.Start();
+            while (th.IsAlive)
+            {
+                Application.DoEvents();
+                Thread.Sleep(500);
+            }
+            Close();
+            Environment.Exit(0);
+        }
+    }
+}
